568a36d8b796391ab2e82660bac5120d80ca101a
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-18 11:54:10 +0530

    For use input

--- OOP/1.teacher.cpp
+++ OOP/1.teacher.cpp
@@ -31,12 +31,14 @@ int main(){
     t1.name = "Siddharth";
     t1.dept = " Computer Science ";
     t1.subject = " C++ ";
-    t1.salary = 350000;
+    // t1.salary = 350000;
+    t1.setSalary(350000);
 
     cout << t1.name << endl;
     cout << t1.dept << endl;
     cout << t1.subject << endl;
-    cout << t1.salary << endl;
+    // cout << t1.salary << endl;
+    cout << t1.getSalary() << endl;
 
 
     return 0;
