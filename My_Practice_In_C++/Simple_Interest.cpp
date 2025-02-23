7edb5b88a9f7151a92506312854fd5e3dbaaaef0
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-23 14:51:53 +0530

    Simple Intreset

---
+++ My_Practice_In_C++/Simple_Interest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float Princeple = 1000;
+    int Rate = 5, Time = 5;
+    int SI = ( Princeple * Rate * Time ) / 100;
+
+    cout << " Simple Interest is = " << SI << endl; // O/P = 250
+
+}
