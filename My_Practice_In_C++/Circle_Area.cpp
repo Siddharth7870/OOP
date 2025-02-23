a0c816d4a4fb580629372148d429461c807e715a
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-23 14:38:45 +0530

    Area of Circle

---
+++ My_Practice_In_C++/Circle_Area.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int radius = 3;
+    float PI = 3.14;
+    float area = ( PI * radius * radius );
+    cout << "Area of circle = " << area ;
+}
